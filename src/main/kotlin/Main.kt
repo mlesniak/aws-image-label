adf03634cbb2229dc68d9195076f3259e9cd5779
Michael Lesniak <mail@mlesniak.com>
2021-03-10 15:36:13 +0100

    List files in S3 bucket

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,25 @@
-class Main {
+import com.amazonaws.regions.Regions
+import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import com.amazonaws.services.s3.model.Bucket
+import com.amazonaws.services.s3.model.ListObjectsV2Result
 
-}
 
 fun main() {
-    println("Hello, world")
+    val s3: AmazonS3 = AmazonS3ClientBuilder
+        .standard()
+        .withRegion(Regions.EU_CENTRAL_1)
+        .build()
+
+    val buckets: List<Bucket> = s3.listBuckets()
+    println("Your Amazon S3 buckets are:")
+    for (b in buckets) {
+        println("* ${b.name}")
+    }
+
+    val result: ListObjectsV2Result = s3.listObjectsV2("com.mlesniak.photos", "test/")
+    val objects = result.objectSummaries
+    for (os in objects) {
+        println("* " + os.key)
+    }
 }
